f552720016095f915250add004661626dd4d0a9c
Md. Al-Imran <asq24i@gmail.com>
2021-08-19 13:38:25 +0600

    some bug fixed

--- WeatherTrack/settings.py
+++ WeatherTrack/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1+g=y1(=3r)rni+7xo&j@^e!asii=-=1t=m&(_$p*=^w388nh#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
